95300032a3621c3126c464b97ca5647070e10c4a
YiHsin Chen <gcobs0834@gmail.com>
2021-11-28 20:36:41 +0800

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'facebookAuth' : {
-		'clientID': '2004338746501769',
-		'clientSecret': '282ac3e6902904cb70fee96d10461345',
+		'clientID': '',
+		'clientSecret': '',
 		'callbackURL': 'http://localhost:8080/user/auth/facebook/callback'
 	},
 }
